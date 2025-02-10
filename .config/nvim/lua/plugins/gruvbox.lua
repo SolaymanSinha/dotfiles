241677ffcbc87d77040a3097a3bbdf4549d29010
Shah Solayman Sinha <md.sinha.p@gmail.com>
2025-02-10 08:56:59 +0600

    config add

---
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,12 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  -- Found in the example.lua. Read more documentation to get more ideas
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
